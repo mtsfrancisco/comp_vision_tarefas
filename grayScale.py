520d97399b92a4bcaed975be2264dfe913974dfb
mtsfrancisco <matheusdabmlj@gmail.com>
2024-07-31 10:24:33 -0300

    more pep8 changes

--- grayScale.py
+++ grayScale.py
@@ -2,36 +2,36 @@ import cv2
 import numpy as np
 
 
-def grayToRgb(gray):
-    altura, largura = gray.shape[:2]
-    img_copy = np.zeros((altura, largura, 3))
-    print(img_copy[0,0])
-    for i in range(altura):
-        for j in range(largura):
+def gray_to_rgb(gray):
+    height, width = gray.shape[:2]
+    img_copy = np.zeros((height, width, 3))
+
+    for i in range(height):
+        for j in range(width):
                 color = gray[i, j]
                 img_copy[i, j] = [color, color, color]
 
     return img_copy.astype(np.uint8)
 
 def grayScale(img):
-    altura, largura = img.shape[:2]
-    img_copy = np.zeros((altura, largura))
+    height, width = img.shape[:2]
+    img_copy = np.zeros((height, width))
 
-    for i in range(altura):
-        for j in range(largura):
-            cor = img[i, j]
-            img_copy[i, j] = 0.2989 * cor[2] + 0.5870 * cor[1] + 0.1140 * cor[0]
+    for i in range(height):
+        for j in range(width):
+            color = img[i, j]
+            img_copy[i, j] = 0.2989 * color[2] + 0.5870 * color[1] + 0.1140 * color[0]
 
     return img_copy.astype(np.uint8)
 
-img = cv2.imread("imagens/image1.webp")
+img = cv2.imread("images/image1.webp")
 img_copy = img.copy()
 gray_copy = cv2.cvtColor(img_copy, cv2.COLOR_BGR2GRAY)
 
 gray = grayScale(img)
-rgb_gray = grayToRgb(gray_copy)
+rgb_gray = gray_to_rgb(gray_copy)
 
-rgb = cv2.cvtColor(gray, cv2.COLOR_GRAY2RGB)
+rgb = cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)
 
 cv2.imshow("RGB Gray", rgb_gray)
 cv2.imshow("RGB", rgb)
